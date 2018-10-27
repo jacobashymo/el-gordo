c3413689cc40b729ee4b818d6231411ba373e2c0
Wesley Kruger <wesleykruger@Wesleys-Device.local>
2018-10-27 11:30:29 -0500

    pushing up everything

--- app.js
+++ app.js
@@ -18,25 +18,25 @@ let tables = [
   }
 ];
 
+app.get("/reservation", function(req, res) {
+  res.sendFile(path.join(__dirname, "reservation.html"));
+});
+
 app.get("/api/tables", function(req, res) {
   return res.json(tables);
 });
 
+app.post("/api/tables", function(req, res) {
+  let newTable = req.body;
 
-  app.post("/api/tables", function(req, res) {
+  console.log("this works");
 
-    let newTable = req.body;
-  
-  
-    console.log("this works");
-  
-    console.log(newTable);
-  
-    tables.push(newTable);
-  
-    res.json(newTable);
-  });
+  console.log(newTable);
 
+  tables.push(newTable);
+
+  res.json(newTable);
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
